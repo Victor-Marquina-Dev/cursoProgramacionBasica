801a001cde1f01cafeb3ad6b27b966d53bc1fb18
Victor Marquina <victor.marquina30@gmail.com>
2025-07-24 14:45:08 -0500

    Subi los archivos a Github

---
+++ cursoProgramacionBasica/proyecto mokepon/js/mokepon.js
@@ -0,0 +1,22 @@
+function iniciarJuego(){
+let botonMascotaJugador = document.getElementById('boton-mascota') 
+botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
+
+function seleccionarMascotaJugador(){
+    let inputHipodogue = document.getElementById('Hipodogue');
+    let inputcapipepo = document.getElementById('Capipepo');
+    let inputRatigueya = document.getElementById('Ratigueya');
+
+
+    if (inputHipodogue.checked) {
+        alert('Seleccionaste a Hipodog');
+    } else if (inputcapipepo.checked) {
+        alert('Seleccionaste a Capipepo');
+    } else if (inputRatigueya.checked) {
+        alert('Seleccionaste a Ratigueya ');
+    }
+}
+
+
+window.addEventListener('load', iniciarJuego);
